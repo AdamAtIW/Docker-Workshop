7040f1253726401c0e9706b32615ca53ae8c05f4
AdamF <adam@iw.com>
2018-05-23 11:47:58 +1200

    init

---
+++ Docker_Comp/website/index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title> Forklifts Shop </title>
+	</head>
+
+
+	<body>
+		<h1>Welcome to Forklifts Shop</h1>
+		<ul>
+			<?php
+				$json = file_get_contents('http://product-service');
+				$obj = json_decode($json);
+
+				$products = $obj->products;
+				foreach ($products as $product) {
+					echo "<li>$product</li>";
+				}
+			?>
+		</ul>
+	</body>
+</html>
